3e022a7b3b2da03e3f750c74e70b370ba7e46be4
ianpaska <168432661+ianpaska@users.noreply.github.com>
2025-09-02 13:17:35 +0200

    Add files via upload

---
+++ App_Start/IdentitySeed.cs
@@ -0,0 +1,66 @@
+﻿using GameShop3.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GameShop3
+{
+    // MANAGER ZA KORISNIKE
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(
+            IdentityFactoryOptions<ApplicationUserManager> options,
+            IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(
+                new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+
+            // VALIDACIJA LOZINKE
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = false,
+                RequireNonLetterOrDigit = false
+            };
+
+            return manager;
+        }
+    }
+
+    // MANAGER ZA PRIJAVU
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(
+            ApplicationUserManager userManager,
+            IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(
+            IdentityFactoryOptions<ApplicationSignInManager> options,
+            IOwinContext context)
+        {
+            return new ApplicationSignInManager(
+                context.GetUserManager<ApplicationUserManager>(),
+                context.Authentication);
+        }
+    }
+}
